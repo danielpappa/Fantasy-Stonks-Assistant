50052cbeeb1a6bbee854d4fd5c5205e808fd8e64
Daniel Pappalardo <142378123+danielpappa@users.noreply.github.com>
2024-04-22 13:06:29 +0200

    Update assistant_model.py

--- assistant_model.py
+++ assistant_model.py
@@ -9,9 +9,9 @@ model = AutoModelForCausalLM.from_pretrained('google/gemma-2b-it') # for GPU: , 
 def augmented_llm(query, history):
   source_information = vector_search.get_search_result(query, mongo_db.get_collection())
   combined_information = (
-      f"Query: {query}\n\nI'm answering based on the following potential match\n{source_information}:"
+      f"Query: {query}\n\nI'm answering based on the following potential match\n{source_information}\nLLM:"
   )
   input_ids = tokenizer(combined_information, return_tensors="pt") # for GPU: .to("cuda")
   response = model.generate(**input_ids, max_new_tokens=500)
-  answer = tokenizer.decode(response[0]).split("\n")[-1].strip(":")
+  answer = tokenizer.decode(response[0]).split("LLM:")[-1]
   return answer
